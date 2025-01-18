57f61b58e515e9a759e85cb591332878d0a8642d
himanisolanki25 <120806213+himanisolanki25@users.noreply.github.com>
2025-01-18 22:37:56 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:4002",
+        target: "https://tavbandhu.onrender.com",
         changeOrigin: true,
       },
     },
