b5768b210fcb5f19925ff798b56c8aa632f68a9a
santosh pal <santu@santoshs-MacBook-Air.local>
2025-01-19 14:41:30 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://tavbandhu.onrender.com",
+        target: "https://tg-app-i3re.onrender.com",
         changeOrigin: true,
       },
     },
